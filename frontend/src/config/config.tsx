3b4bac440c7e507f3498ba7da52ba17526d361da
Parthasarathi Bhattacharya <partha@Parthasarathis-MacBook-Air.local>
2025-09-24 19:39:32 +0530

    Deploy changes 2

--- frontend/src/config/config.tsx
+++ frontend/src/config/config.tsx
@@ -1,5 +1,5 @@
-// export const VITE_BACKEND_URL = "https://civic-issue-reporter-application.onrender.com";
+export const VITE_BACKEND_URL = "https://civiceyed.onrender.com/api";
 // //"http://localhost:5000"
 
-export const VITE_BACKEND_URL = "http://localhost:3000";
+// export const VITE_BACKEND_URL = "http://localhost:3000";
 
