6c6dd3f429bd6a560810a9d04f2a5c71a234c6aa
Parthasarathi Bhattacharya <partha@Parthasarathis-MacBook-Air.local>
2025-09-25 01:17:28 +0530

    Remove .env.production from repository

--- frontend/src/config/config.tsx
+++ frontend/src/config/config.tsx
@@ -1,4 +1,4 @@
-export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string
 // //"http://localhost:5000"
 
 // export const VITE_BACKEND_URL = "http://localhost:3000";
